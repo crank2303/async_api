5511524d1d7b0ada919cdc65fb09a8ca74d212f5
Alexey <aa.reukov@gmail.com>
2023-01-14 21:23:14 +1100

    Person_model append validators

--- etl/models/person_model.py
+++ etl/models/person_model.py
@@ -1,11 +1,24 @@
 from uuid import UUID
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
+from pydantic.schema import Optional
 
 
 class Person(BaseModel):
     id: UUID
     full_name: str
-    role: str
-    film_ids: list(UUID)
+    role: Optional[str]
+    film_ids: Optional[list]
+
+    @validator('role')
+    def valid_role(cls, v):
+        if v is None:
+            return ''
+        return v
+
+    @validator('film_ids')
+    def valid_film_ids(cls, v):
+        if v is None:
+            return []
+        return v
 
 
