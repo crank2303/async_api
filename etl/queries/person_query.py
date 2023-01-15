0d36161223bc8274be96eaf7f76c6c6bd5d3cdcb
Komolov Artem <crank2303@gmail.com>
2023-01-15 13:57:00 +0300

    array_agg changed to json_agg

--- etl/queries/person_query.py
+++ etl/queries/person_query.py
@@ -3,7 +3,7 @@ QUERY_PERSON = """
         p.id,
         p.full_name,
         pfw.role,
-        ARRAY_AGG(DISTINCT fw.id) 
+        JSON_AGG(DISTINCT fw.id) 
     FROM content.person p 
     LEFT JOIN "content".person_film_work pfw  on pfw.person_id = p.id
     LEFT JOIN "content".film_work fw  on fw.id = pfw.film_work_id 
